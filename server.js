ea10e60ac28ca311e8809d7f0d1ec58165bd4e8f
bharathP30 <bharath145p@gmail.com>
2026-04-24 08:31:31 +0530

    fix cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import todoRoute from "./routes/todoRoute.js";
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5173", "https://task-manager-v2-frontend-seven.vercel.app/"],
+    origin: ["http://localhost:5173", "https://task-manager-v2-frontend-seven.vercel.app"],
 }));
 app.use(express.json());
 
